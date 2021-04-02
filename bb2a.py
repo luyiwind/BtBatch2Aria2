295cb28772724ac4c1716bae6947c72fe2c5665d
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-04-02 13:38:58 +0800

    Update bb2a.py

--- bb2a.py
+++ bb2a.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     parser.add_argument("dir", help="the dir of your bittorrents", type=str)
     parser.add_argument("mgdir", help="the dir of your magnets", type=str)
     parser.add_argument("secret", help="secrets", type=str)
-    parser.add_argument("name", help="mag name", type=str)
+    #parser.add_argument("name", help="mag name", type=str)
     args = parser.parse_args()
     s = xmlrpc.client.ServerProxy(args.server+"rpc")
     flist=os.listdir(args.dir)
@@ -65,12 +65,12 @@ if __name__ == "__main__":
             
     for root, dirs, files in os.walk(args.mgdir):
         for file in files:
-            if args.name.lower().replace('-','') in file.lower().replace('-',''):
-                if file.endswith(".txt"):
-                    count += 1
-                    if count > args.num:
-                        continue
-                    mgFile = os.path.join(root, file)
-                    handleMag(s,mgFile,args.secret)
+            #if args.name.lower().replace('-','') in file.lower().replace('-',''):
+            if file.endswith(".txt"):
+                count += 1
+                if count > args.num:
+                    continue
+                mgFile = os.path.join(root, file)
+                handleMag(s,mgFile,args.secret)
     
     print("Done")
