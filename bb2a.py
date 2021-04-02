4d8cf89ac532926d1e3194b8c06d9cc7398180ce
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-04-02 13:55:39 +0800

    Update bb2a.py

--- bb2a.py
+++ bb2a.py
@@ -77,4 +77,5 @@ if __name__ == "__main__":
                         continue
                     mgFile = os.path.join(root, file)
                     handleMag(s,mgFile,args.secret)
+                    
     print("Done")
